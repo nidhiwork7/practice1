8708e9106149e98c190552755dc942bc97c231b6
nidhikumari906 <nidhi.kumari@droom.in>
2018-04-09 17:55:28 +0530

    LL-2 | saving form data in mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://127.0.0.1/my_mongo");
+var UserSchema = new mongoose.Schema({
+    firstName: String,
+ 	lastName: String
 });
+var User = mongoose.model("User", UserSchema);
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Droom' });
+  // res.json({ user: 'tobi' });
+});
+router.post('/addname', function (req, res) {
+	var personInfo = req.body;
+	var UserSchema = new User({
+	    firstName: personInfo.firstname,
+	 	lastName: personInfo.firstname
+	});
+		
+      UserSchema.save(function(err, User){
+         if(err)
+            res.render('show_message', {message: "Database error", type: "error"});
+         else
+            res.render('show_message', {
+               message: "New person added", type: "success", person: personInfo});
+      });
+})
 
 module.exports = router;
