6ee89c5754912d76ff5a06e52235f0f1ae69dd67
nidhikumari906 <nidhi.kumari@droom.in>
2018-04-18 19:16:07 +0530

    LL-5 | connecting two tables data in mongodb

--- routes/index.js
+++ routes/index.js
@@ -7,33 +7,51 @@ mongoose.connect("mongodb://127.0.0.1/my_mongo");
 
 //Defining schemas
 var UserSchema = new mongoose.Schema({
-    firstName: {
-    	type: String,
-        min: [6, 'Too few eggs'],
-        max: 12,
-        required: [true, 'Please enter first name']
-    },
- 	lastName: {
- 		type: String,
- 		enum: ['Coffee', 'Tea', 'Water',],
-        required: true
-    }
+    firstName: String,
+    userData : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Customer' }]
+});
+var CustomerSchema = new mongoose.Schema({
+    name: String,
+    address: String
 });
 // Compile model from schema
 var User = mongoose.model("User", UserSchema);
+var Customer = mongoose.model("Customer", CustomerSchema);
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Droom' });
   // res.json({ user: 'tobi' });
 });
 router.get('/showData', function (req, res) {
-	var query = User.find({'lastName':'Coffee'}, 'firstName lastName').sort({ firstName: 1 });
-	query.exec( function(err, User){
+	var CustomerSchema = new Customer({
+		name: 'CustomerUser9',
+		address: 'CustomerUser Address8'
+	});
+	CustomerSchema.save(function(err, Customer){
 	    if(err)
 	        res.render('show_message', {message: "Database error", type: "error"});
-	    else
-	        res.render('show_message', {message: "New person added", type: "success", person: User});
-      });
-})
+	    else{
+	    	var UserSchema = new User({
+	    		firstName: "CustomerUser9",
+	    		userData: CustomerSchema._id
+	    	});
+	    	UserSchema.save(function(err, data){
+	    		if(err)
+			        res.render('show_message', {message: "Database error", type: "error"});
+			    else{
+			    	User.find({ firstName: "CustomerUser9" })
+					.populate('userData')
+					.exec(function (err, data) {
+						console.log(data[0].userData);
+						if(err)
+			        		res.render('show_message', {message: "Database error", type: "error"});
+					  console.log('The address is %s', data[0].userData[0].address);
+					});
+			    	res.render('show_message', {message: "New person added", type: "success"});			    	
+			    }
+	    	});
+	    }
+	});
+});
 
 module.exports = router;
